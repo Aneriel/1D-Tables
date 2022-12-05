8703bd089845ee2811238aed0f205579199fac3e
Aneriel <bartlomiej.kaczmarsk@microsoft.wsei.edu.pl>
2022-12-05 14:06:04 +0100

    walka z kodem

--- 1D-Tables/Program.cs
+++ 1D-Tables/Program.cs
@@ -7,10 +7,20 @@ namespace _1D_Tables
         {
             int[] a;
             int[] b;
+           // int counter = 0;
             a = new int[] { 0, 1, 1, 2, 3, 3, 3 };
             b = new int[] { 0, 2, 2, 3, 4 };
+            //int[] distinctArr = a;
+
             var test = false;
-            foreach(int i in a)
+            /*foreach (int distinctVal in a)
+            {
+                if(Array.IndexOf(distinctArr, distinctVal) == 0)
+                {
+                    Console.Write($"{distinctVal} ");
+                }
+            }*/
+           foreach (int i in a)
             {
 
                 if (Array.IndexOf(b, i) < 0)
@@ -18,13 +28,14 @@ namespace _1D_Tables
                     Console.Write($"{i} ");
                     test = true;
                 }
-                
-                
+
+
             }
-            if(test == false)
+            if (test == false)
             {
                 Console.WriteLine("empty");
             }
+
         }
     }
 }
