0aa7676755a37b9af61a4eeaf7ac66a6e20a170b
Aneriel <bartlomiej.kaczmarsk@microsoft.wsei.edu.pl>
2022-12-05 13:12:42 +0100

    Add project files.

---
+++ 1D-Tables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace _1D_Tables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a;
+            int[] b;
+            a = new int[] { 0, 1, 1, 2, 3, 3, 3 };
+            b = new int[] { 0, 2, 2, 3, 4 };
+            var test = false;
+            foreach(int i in a)
+            {
+
+                if (Array.IndexOf(b, i) < 0)
+                {
+                    Console.Write($"{i} ");
+                    test = true;
+                }
+                
+                
+            }
+            if(test == false)
+            {
+                Console.WriteLine("empty");
+            }
+        }
+    }
+}
